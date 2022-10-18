f3a056675a398ed94a8c15940eaa579c5b4bf895
sellauno <sellauno03@gmail.com>
2022-10-18 21:18:50 +0700

    create

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pydoc import doc
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+import datetime
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.gfvlmzn.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -11,8 +12,22 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
+@app.route("/create", methods=["POST"])
+def create_data():
+    # sample_receive = request.form['sample_give']
+    mydate = request.form['mydate']
+    mytime = request.form['mytime']
 
+    num = datetime.datetime.now()
+    num.isoformat()
 
+    doc = {
+        'num': num,
+        'date': mydate,
+        'time': mytime,
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': 'data saved!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
