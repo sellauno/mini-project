030cab98c8a41b4853d4f056c059e9ed85097d7a
Ahmad Hasan Ali <ahmadhasanali24@student.uns.ac.id>
2022-10-18 20:29:08 +0700

    base app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from pydoc import doc
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.gfvlmzn.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
